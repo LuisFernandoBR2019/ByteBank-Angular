a0efd8496fbc073caf64f80fe548d1c66bb6fb50
Luis Fernando <nando_srs@hotmail.com>
2021-07-27 21:41:33 -0300

    Adicionado SCSS e capturando dados do formulario e printando no console log

--- src/app/nova-transferencia/nova-transferencia-component.ts
+++ src/app/nova-transferencia/nova-transferencia-component.ts
@@ -1,10 +1,16 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector: 'nova-transferencia',
-    templateUrl: './nova-transferencia-component.html',
-    styleUrls:['nova-transferencia-component.scss']
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia-component.html',
+  styleUrls: ['nova-transferencia-component.scss']
 })
-export class NovaTransferenciaComponent{
-    
+export class NovaTransferenciaComponent {
+
+  valor:number = 0;
+  destino:number = 0;
+
+  transferir() {
+    console.log('Trasferencia realizada. \n Valor: '+ this.valor +' \n Destino: '+ this.destino);
+  }
 }
