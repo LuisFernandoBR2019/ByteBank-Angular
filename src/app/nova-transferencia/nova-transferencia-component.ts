6983f3a2239aae80020d184e25a75267b5bd4875
Luis Fernando <nando_srs@hotmail.com>
2021-07-28 22:21:22 -0300

    Adicionado Service, componente extrato e locale pt para data e hora / currency BRL

--- src/app/nova-transferencia/nova-transferencia-component.ts
+++ src/app/nova-transferencia/nova-transferencia-component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { EventEmitter } from "@angular/core";
+import { Component, Output } from "@angular/core";
 
 @Component({
   selector: 'app-nova-transferencia',
@@ -7,10 +8,21 @@ import { Component } from "@angular/core";
 })
 export class NovaTransferenciaComponent {
 
+  @Output() aoTransferir = new EventEmitter<any>();
   valor:number = 0;
   destino:number = 0;
 
   transferir() {
     console.log('Trasferencia realizada. \n Valor: '+ this.valor +' \n Destino: '+ this.destino);
+    const valorEmitir = {valor : this.valor, destino : this.destino};
+    this.aoTransferir.emit(valorEmitir);
+
+    this.limparCampos();
   }
+
+  limparCampos(){
+    this.valor = 0;
+    this.destino = 0;
+  }
+
 }
